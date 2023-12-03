b1c0d384584f1dd5d2049f615b91af81500820f8
Pangolecimal <domkuzaleza@gmail.com>
2023-12-03 04:23:59 +0300

    figured out filesystem routing

---
+++ src/routes/+layout.server.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+
+export async function load() {
+	let routes = ['./src/routes'];
+
+	for (let i = 0; i < 5; i++) {
+		routes = routes.flatMap((path) => {
+			let result;
+			try {
+				result = fs.readdirSync(path);
+			} catch {
+				return path;
+			}
+			if (result.length == 0) return path;
+			return result.flatMap((p) => path + '/' + p);
+		});
+	}
+
+	routes = routes.flatMap((r) => {
+		let l = r.lastIndexOf('/');
+		return r.slice('./src/routes'.length, l) || '/'; // `||` instead of `??` to leave '' as '/'
+	});
+
+	routes = Array.from(new Set(routes));
+
+	return {
+		routes,
+	};
+}
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { page } from '$app/stores';
+
+	/** @type {import('./$types').LayoutData} */
+	export let data;
+</script>
+
+{#each data.routes as route}
+	<a href={route}>GOTO {route}</a><br /><br />
+{/each}
+Curreantly at: {$page.url.pathname}
+<slot />
