9340d13074aa96525692c4650a9a14ef2543049e
Pangolecimal <domkuzaleza@gmail.com>
2023-12-07 21:14:43 +0300

    more #2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,17 @@
 <script>
-	import { page } from '$app/stores';
+	// import { onMount } from 'svelte';
 
 	/** @type {import('./$types').LayoutData} */
 	export let data;
 
-	// let max_depth = 0;
+	let max_depth = 0;
 	let routes = data.routes.map((route) => {
 		if (route === '/') return { name: 'Home', depth: 1, href: route };
 
 		let route_path = route.split('/');
 		let route_end = route_path.at(-1) ?? '';
 		let route_depth = route_path.length;
-		// max_depth = Math.max(max_depth, route_depth);
+		max_depth = Math.max(max_depth, route_depth);
 
 		return {
 			name: route_end
@@ -21,44 +21,75 @@
 					return r[0].toUpperCase() + r.slice(1);
 				})
 				.join(' '),
-			depth: route_depth,
 			href: route,
+			depth: route_depth,
 		};
 	});
 
-	/* File Tree
-> Home
-> > About
-> > My Projects
-> > > Algorithms
-> > > > Delaunay
-> > > Configs
-> > > Games
-> > > > Lights Out
-> > Op Projects
-> > > Articles
-> > > Configs
-> > > Websites
-*/
+	let /** @type {HTMLElement} */ header;
+	let /** @type {HTMLElement} */ open;
+	let /** @type {HTMLElement} */ close;
+	function close_header() {
+		header.dataset.active = 'false';
+		open.dataset.active = 'true';
+	}
+	function open_header() {
+		header.dataset.active = 'true';
+		open.dataset.active = 'false';
+	}
+
+	let ft = '';
+	ft += '<ul>';
+	for (let i = 0; i < routes.length; i++) {
+		// for (let i = 0; i < max_depth; i++) {
+		// let routes_i = routes.filter((r) => r.href.split('/').length === i);
+
+		ft += '<li>';
+		ft += '<a href="' + routes[i].href;
+		ft += `" style="padding-left: ${routes[i].depth * 1.5}rem">` + routes[i].name;
+		ft += '</a>';
+	}
+	ft += '</ul>';
 </script>
 
+<!-- {#each routes as route, i}
+	<div
+		class="file"
+		data-depth={route.depth}
+		data-active={true}
+		data-index={i}
+		style="padding-left: {route.depth * 2}rem;"
+	>
+		{#if route.depth < max_depth}
+			<i class="fa-solid fa-folder" />
+		{:else}
+			<i class="fa-solid fa-file" />
+		{/if}
+		<a href={route.href}>
+			{route.name}
+		</a>
+	</div>
+{/each} -->
+
 <div id="wrapper">
-	<header>
-		<div id="filetree">
-			{#each routes as route}
-				<div class="file">
-					<a href={route.href} data-depth={route.depth} data-active={true}>{route.name}</a>
-				</div>
-			{/each}
-		</div>
+	<header data-active={true} bind:this={header}>
+		<div id="filetree">{@html ft}</div>
 
 		<!-- Settings, etc. -->
 		<div id="settings">
 			<i class="fa-solid fa-cog" />
+			<button id="settings-close" data-active="true" on:click={close_header} bind:this={close}>
+				<i class="fa-solid fa-square-chevron-left" />
+			</button>
 		</div>
 	</header>
 
-	<main><slot /></main>
+	<main>
+		<button id="settings-open" on:click={open_header} data-active="false" bind:this={open}>
+			<i class="fa-solid fa-square-chevron-right" />
+		</button>
+		<slot />
+	</main>
 </div>
 
 <style>
@@ -79,6 +110,11 @@
 		color: var(--ctp-mocha-peach);
 		font-weight: bold;
 
+		overflow: hidden;
+		white-space: nowrap;
+		text-overflow: clip;
+		text-wrap: nowrap;
+
 		&:hover {
 			text-decoration: underline;
 			color: var(--ctp-mocha-pink);
@@ -91,6 +127,8 @@
 	:global(button) {
 		border: none;
 		transition: filter 100ms;
+		background: inherit;
+		color: inherit;
 		&:hover {
 			filter: brightness(115%);
 		}
@@ -107,75 +145,107 @@
 		user-select: none;
 	}
 
+	:global([data-active='true']) {
+		opacity: 100%;
+		visibility: visible;
+	}
+	:global([data-active='false']) {
+		opacity: 0;
+		visibility: hidden;
+	}
+
 	/*  */
 
 	#wrapper {
 		display: grid;
 		grid-template-columns: auto 1fr;
 		height: 100%;
+		position: relative;
+	}
+
+	main {
+		position: relative;
 	}
 
 	header {
 		display: grid;
-		position: relative;
 
-		width: 30ch;
+		width: 24rem;
 		height: 100%;
 
 		font-size: 1.25rem;
 		padding: 1rem;
 		background: var(--ctp-mocha-mantle);
 
-		overflow-y: scroll;
+		transition: 1s;
+	}
+	:global(header[data-active='false']) {
+		width: 0;
+		padding: 0;
 	}
 
 	#filetree {
-		height: fit-content;
-		display: grid;
+		height: 100%;
+		/* display: grid; */
 		padding-bottom: 3rem;
+		position: relative;
 	}
-	.file {
+	/* .file {
 		padding-top: 1rem;
-	}
-	[data-depth='1'] {
-		padding-left: 1rem;
-	}
-	[data-depth='2'] {
-		padding-left: 2rem;
-	}
-	[data-depth='3'] {
-		padding-left: 3rem;
-	}
-	[data-depth='4'] {
-		padding-left: 4rem;
-	}
-	[data-depth='5'] {
-		padding-left: 5rem;
-	}
+		overflow: hidden;
+	} */
 
 	#settings {
 		display: grid;
+		grid-auto-flow: column;
 		place-items: center;
 		align-self: end;
 
-		position: sticky;
+		position: absolute;
+		left: 0;
+		bottom: 0;
+		width: 24rem;
 
-		padding: 0.5rem;
+		padding: 1rem;
 		font-size: 2rem;
 		background: var(--ctp-mocha-crust);
+	}
+	#settings-close {
+		position: absolute;
+		right: 0;
+
+		font-size: 2rem;
+		padding: 0.5rem;
+		background: var(--ctp-mocha-crust);
+		color: var(--ctp-mocha-text);
+
+		transition: 1s;
+	}
+	#settings-open {
+		position: absolute;
+		left: 0;
+		bottom: 0.5rem;
+
+		border-radius: 0 1rem 1rem 0;
+		font-size: 2rem;
+		padding: 0.5rem;
+		background: var(--ctp-mocha-mantle);
+		color: var(--ctp-mocha-text);
+
+		transition: 1s;
 	}
 
 	/*  */
 
-	@media only screen and (orientation: portrait) {
+	/* @media only screen and (orientation: portrait) {
 		#wrapper {
 			grid-auto-flow: column;
 			grid-template-columns: 1fr;
 			grid-template-rows: auto 1fr;
 		}
 
-		#filetree {
+		header {
 			width: 100%;
 		}
-	}
+	} */
 </style>
